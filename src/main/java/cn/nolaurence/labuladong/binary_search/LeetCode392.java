d6e19558e2b6687d4a887c5eff34bc473c244456
nolaurence <nolaurence@outlook.com>
2024-04-20 20:17:19 +0800

    feat: more

---
+++ src/main/java/cn/nolaurence/labuladong/binary_search/LeetCode392.java
@@ -0,0 +1,12 @@
+package cn.nolaurence.labuladong.binary_search;
+
+public class LeetCode392 {
+
+    /**
+     * 给定字符串s和t，判断s是否为t的子序列
+     * 进阶：如果有大量输入的S，称做S1，S2，...，Sk其中k > 10亿，你需要一次检查他们
+     * 是否为T的子序列。在这种情况下，你会怎样改变代码
+     */
+
+
+}
